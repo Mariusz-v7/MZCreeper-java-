9948f688ab50d96b80f418ee18c34b9c84e29bea
Mariusz Gęzicki <mariusz.v7@gmail.com>
2016-06-11 19:36:47 +0200

    Change the user agent value and set window resolution

--- src/main/java/pl/mrugames/mzcreeper/MainConfiguration.java
+++ src/main/java/pl/mrugames/mzcreeper/MainConfiguration.java
@@ -1,5 +1,6 @@
 package pl.mrugames.mzcreeper;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
@@ -12,14 +13,18 @@ import org.springframework.core.env.Environment;
 public class MainConfiguration {
     @Bean
     public DesiredCapabilities desiredCapabilities(Environment environment) {
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        DesiredCapabilities desiredCapabilities = DesiredCapabilities.phantomjs();
         desiredCapabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, environment.getProperty("webdriver.path"));
+        desiredCapabilities.setCapability(PhantomJSDriverService.PHANTOMJS_PAGE_SETTINGS_PREFIX + "userAgent", environment.getProperty("webdriver.user_agent"));
 
         return desiredCapabilities;
     }
 
     @Bean
     public WebDriver webDriver(DesiredCapabilities desiredCapabilities) {
-        return new PhantomJSDriver(desiredCapabilities);
+        PhantomJSDriver driver = new PhantomJSDriver(desiredCapabilities);
+        driver.manage().window().setSize(new Dimension(1366, 768));
+
+        return driver;
     }
 }
