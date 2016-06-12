6ef614323f41e137d83886ff08c0c9a12c0b279d
Mariusz Gęzicki <mariusz.v7@gmail.com>
2016-06-12 21:33:24 +0200

    Add planned matches loader

--- src/main/java/pl/mrugames/mzcreeper/Link.java
+++ src/main/java/pl/mrugames/mzcreeper/Link.java
@@ -1,10 +1,12 @@
 package pl.mrugames.mzcreeper;
 
+import pl.mrugames.mzcreeper.parsers.ChallengesParser;
 import pl.mrugames.mzcreeper.parsers.MainPageParser;
 import pl.mrugames.mzcreeper.parsers.Parser;
 
 public enum Link {
-    MAIN_PAGE("http://www.managerzone.com/", MainPageParser.class);
+    MAIN_PAGE("http://www.managerzone.com/", MainPageParser.class),
+    CHALLENGES("http://www.managerzone.com/?p=challenges", ChallengesParser.class);
 
     private final String link;
     private final Class<? extends Parser> parser;
---
+++ src/main/java/pl/mrugames/mzcreeper/parsers/ChallengesParser.java
@@ -0,0 +1,56 @@
+package pl.mrugames.mzcreeper.parsers;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import pl.mrugames.mzcreeper.Link;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ChallengesParser implements Parser {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final WebDriver webDriver;
+
+    private final static String PLANNED_MATCHES_TABLE_ID = "my_booked_friendlies";
+    private final static int PLANED_MATCH_DATE_COLUMN_INDEX = 2;
+    private final static DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+
+    @Autowired
+    public ChallengesParser(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    @Override
+    public void parse() {
+        webDriver.get(Link.CHALLENGES.getLink());
+
+        List<LocalDateTime> plannedDates = loadInfoAboutCurrentlyPlannedMatches();
+        // TODO: calculate days without matches planned yet (calculate dates for whole week and subtract planned dates)
+    }
+
+    private List<LocalDateTime> loadInfoAboutCurrentlyPlannedMatches() {
+        WebElement plannedMatchesTable = webDriver.findElement(By.id(PLANNED_MATCHES_TABLE_ID)).findElement(By.tagName("tbody"));
+        List<WebElement> plannedMatchesRows = plannedMatchesTable.findElements(By.tagName("tr"));
+
+        logger.info("You have currently {} planned matches", plannedMatchesRows.size());
+
+        return getDatesOfPlannedMatches(plannedMatchesRows);
+    }
+
+    private List<LocalDateTime> getDatesOfPlannedMatches(List<WebElement> plannedMatchesRows) {
+        return plannedMatchesRows.stream()
+                .map(row -> row.findElements(By.tagName("td")).get(PLANED_MATCH_DATE_COLUMN_INDEX))
+                .map(WebElement::getText)
+                .map(date -> LocalDateTime.parse(date, DATE_FORMATTER))
+                .collect(Collectors.toCollection(LinkedList::new));
+    }
+}
